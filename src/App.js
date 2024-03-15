2af4d6f308debe7a36afe2877e0ef86ae3271343
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-03-15 11:24:48 -0300

    creating component Card and mapping json

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
+import Card from "./components/Card";
+import videos from "./json/db.json";
 
 function App() {
   return (
     <>
       <Header />
-      <Banner image="assistir"/>
+      <Banner image="home" />
       <Container>
-        <h1>Hello world!</h1>
-        <p>Estou estudando React JS</p>
+        <h2>Categoria!</h2>
+        <section className="cards">
+          {videos.map((video) => {
+            return <Card id={video.id} key={video.id} />;
+          })}
+        </section>
       </Container>
       <Footer />
     </>
