692a8e1b6e48b40b6974f4d733349880c5c87c3a
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-03-19 09:05:19 -0300

    creating category component and filtering categories

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+import Category from "./components/Category";
+
+const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia",
+];
+
+function filterCategory(id) {
+  return videos.filter((video) => video.category === categories[id]);
+}
 
 function App() {
   return (
@@ -11,12 +24,31 @@ function App() {
       <Header />
       <Banner image="home" />
       <Container>
-        <h2>Categoria!</h2>
-        <section className="cards">
-          {videos.map((video) => {
+        <Category category={categories[0]}>
+          {filterCategory(0).map((video) => {
             return <Card id={video.id} key={video.id} />;
           })}
-        </section>
+        </Category>
+        <Category category={categories[1]}>
+          {filterCategory(1).map((video) => {
+            return <Card id={video.id} key={video.id} />;
+          })}
+        </Category>
+        <Category category={categories[2]}>
+          {filterCategory(2).map((video) => {
+            return <Card id={video.id} key={video.id} />;
+          })}
+        </Category>
+        <Category category={categories[3]}>
+          {filterCategory(3).map((video) => {
+            return <Card id={video.id} key={video.id} />;
+          })}
+        </Category>
+        <Category category={categories[4]}>
+          {filterCategory(4).map((video) => {
+            return <Card id={video.id} key={video.id} />;
+          })}
+        </Category>
       </Container>
       <Footer />
     </>
