697ce182b89ad110d929a19154a0f51252002446
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-03-19 09:59:22 -0300

    removing excess code in the json mapping and exporting and importing functions and constants

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import videos from "./json/videos.json";
-import Category from "./components/Category";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Meteorologia, Vegetação",
-  "Geologia e Hidrografia",
-];
-
-function filterCategory(id) {
-  return videos.filter((video) => video.category === categories[id]);
-}
+import Category, { categories, filterCategory } from "./components/Category";
 
 function App() {
   return (
@@ -24,31 +11,13 @@ function App() {
       <Header />
       <Banner image="home" />
       <Container>
-        <Category category={categories[0]}>
-          {filterCategory(0).map((video) => {
-            return <Card id={video.id} key={video.id} />;
-          })}
-        </Category>
-        <Category category={categories[1]}>
-          {filterCategory(1).map((video) => {
-            return <Card id={video.id} key={video.id} />;
-          })}
-        </Category>
-        <Category category={categories[2]}>
-          {filterCategory(2).map((video) => {
-            return <Card id={video.id} key={video.id} />;
-          })}
-        </Category>
-        <Category category={categories[3]}>
-          {filterCategory(3).map((video) => {
-            return <Card id={video.id} key={video.id} />;
-          })}
-        </Category>
-        <Category category={categories[4]}>
-          {filterCategory(4).map((video) => {
-            return <Card id={video.id} key={video.id} />;
-          })}
-        </Category>
+        {categories.map((category, index) => (
+          <Category category={category}>
+            {filterCategory(index).map((video) => {
+              return <Card id={video.id} key={video.id} />;
+            })}
+          </Category>
+        ))}
       </Container>
       <Footer />
     </>
