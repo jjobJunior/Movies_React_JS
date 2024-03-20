ced81b7b0e335df6d7533b85fd8c4a7aa24dcbba
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-03-20 11:02:05 -0300

    creating dynamic routes, and displaying video by card ID

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,8 +1,20 @@
+import { useParams } from "react-router-dom";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import styles from "./Watch.module.css";
+import videos from "../../json/videos.json";
+import PageNotFound from "../PageNotFound";
+
 function Watch() {
+  const params = useParams();
+  const video = videos.find((video) => {
+    return video.id === params.id;
+  });
+  if (!video) {
+    return <PageNotFound />;
+  }
+
   return (
     <>
       <Header />
@@ -11,7 +23,7 @@ function Watch() {
           <iframe
             width="854"
             height="480"
-            src="https://www.youtube.com/embed/ZfCV8m97wG4?si=h6raIJvtrB1Q7Gxp"
+            src={`https://www.youtube.com/embed/${video.id}`}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
